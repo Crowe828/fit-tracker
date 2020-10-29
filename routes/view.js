1754dd2321de92f396d1d55f75b017669e39fb9d
frillic <69489498+crowe828@users.noreply.github.com>
2020-10-29 16:39:14 -0400

    working on models and routes

--- routes/view.js
+++ routes/view.js
@@ -1,1 +1,7 @@
 // Serve up HTML files
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
