167bc30a9c7c56e99f63e3bc0ab2ddee78465a27
frillic <69489498+crowe828@users.noreply.github.com>
2020-10-29 21:40:05 -0400

    navigation between pages now works

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,14 @@
 const router = require("express").Router();
 const Workout = require("../models/workout");
 
-module.exports = function (app) {
-  router.post("/api/workouts", (req, res) => {
-    Workout.create({})
-      .then((dbWorkout) => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
-};
+router.post("/api/workouts", (req, res) => {
+  Workout.create({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
-// Add exercises to a previous workout plan.
-// Add new exercises to a new workout plan.
-// View the combined weight of multiple exercises on the `stats` page.
+module.exports = router;
--- routes/view.js
+++ routes/view.js
@@ -1,10 +1,13 @@
-module.exports = function (app) {
-  // Exercise
-  router.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/exercise.html"));
-  });
-  // Stats
-  router.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/stats.html"));
-  });
-};
+const router = require("express").Router();
+const path = require("path");
+
+// Exercise
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+// Stats
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+module.exports = router;
